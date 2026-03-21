199050650c98db3bb549c3881cc7af0e963f397a
NadeekaDilhan <NadeekaD@AxionSolutions.Biz>
2026-03-21 10:25:32 +0900

    add github pck publish

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -65,13 +65,13 @@ publishing {
     }
     repositories {
         mavenLocal()
-//        maven {
-//            name = "GitHubPackages"
-//            url = uri("https://maven.pkg.github.com/Nadee95/nadeex-spring-common")
-//            credentials {
-//                username = System.getenv("GITHUB_ACTOR")
-//                password = System.getenv("GITHUB_TOKEN")
-//            }
-//        }
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Nadee95/nadeex-spring-common")
+            credentials {
+                username =  System.getenv("GITHUB_ACTOR")
+                password =  System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
